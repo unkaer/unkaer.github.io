e4e494db12fceb7472819e5d0b95d60121c38a64
unkaer <ababwbq@outlook.com>
2021-03-07 13:49:39 +0000

    Site updated: 2021-03-07 13:49:39

---
+++ vip/style/style/css/jiexi.js
@@ -0,0 +1,40 @@
+var __encode = 'sojson.com',
+    _0xb483 = ["_decode", "http://www.sojson.com/javascriptobfuscator.html"];
+(function(_0xd642x1) {
+    _0xd642x1[_0xb483[0]] = _0xb483[1]
+})(window);
+var __Ox20eee = ["b a(){0 6=1.2("9").4;0 5=1.2("3");0 3=1.2("3").c;0 8=5.e[3].4;0 7=1.2("f");7.d=8+6}", "|", "split", "var|document|getElementById|jk|value|jkurl|diz|cljurl|jkv|url|dihejk|function|selectedIndex|src|options|player", "", "fromCharCode", "replace", "\w+", "\b", "g", "value", "url", "getElementById", "jk", "selectedIndex", "options", "player", "_blank", "open"];
+eval(function(_0x26b4x1, _0x26b4x2, _0x26b4x3, _0x26b4x4, _0x26b4x5, _0x26b4x6) {
+    _0x26b4x5 = function(_0x26b4x3) {
+        return (_0x26b4x3 < _0x26b4x2 ? __Ox20eee[4] : _0x26b4x5(parseInt(_0x26b4x3 / _0x26b4x2))) + ((_0x26b4x3 = _0x26b4x3 % _0x26b4x2) > 35 ? String[__Ox20eee[5]](_0x26b4x3 + 29) : _0x26b4x3.toString(36))
+    };
+    if (!__Ox20eee[4][__Ox20eee[6]](/^/, String)) {
+        while (_0x26b4x3--) {
+            _0x26b4x6[_0x26b4x5(_0x26b4x3)] = _0x26b4x4[_0x26b4x3] || _0x26b4x5(_0x26b4x3)
+        };
+        _0x26b4x4 = [
+            function(_0x26b4x5) {
+                return _0x26b4x6[_0x26b4x5]
+            }
+        ];
+        _0x26b4x5 = function() {
+            return __Ox20eee[7]
+        };
+        _0x26b4x3 = 1
+    };
+    while (_0x26b4x3--) {
+        if (_0x26b4x4[_0x26b4x3]) {
+            _0x26b4x1 = _0x26b4x1[__Ox20eee[6]](new RegExp(__Ox20eee[8] + _0x26b4x5(_0x26b4x3) + __Ox20eee[8], __Ox20eee[9]), _0x26b4x4[_0x26b4x3])
+        }
+    };
+    return _0x26b4x1
+}(__Ox20eee[0], 16, 16, __Ox20eee[3][__Ox20eee[2]](__Ox20eee[1]), 0, {}));
+
+function dihejk2() {
+    var _0x26b4x8 = document[__Ox20eee[12]](__Ox20eee[11])[__Ox20eee[10]];
+    var _0x26b4x9 = document[__Ox20eee[12]](__Ox20eee[13]);
+    var _0x26b4xa = document[__Ox20eee[12]](__Ox20eee[13])[__Ox20eee[14]];
+    var _0x26b4xb = _0x26b4x9[__Ox20eee[15]][_0x26b4xa][__Ox20eee[10]];
+    var _0x26b4xc = document[__Ox20eee[12]](__Ox20eee[16]);
+    window[__Ox20eee[18]](_0x26b4xb + _0x26b4x8, __Ox20eee[17])
+}
---
+++ vip/style/style/css/ok.js
@@ -0,0 +1,12 @@
+var __encode = 'sojson.com',
+    _0xb483 = ["_decode", "http://www.sojson.com/javascriptobfuscator.html"];
+(function(_0xd642x1) {
+    _0xd642x1[_0xb483[0]] = _0xb483[1]
+})(window);
+var __Ox20ef1 = ["match", "userAgent", "<iframe  src='https://gitee.com/Unkaer/blog/raw/master/videos/jz.mp4' id='player' width='100%' height='300px' allowTransparency='true' allowfullscreen='true' frameborder='0' scrolling='no'> </iframe>", "writeln", "<iframe  src='https://gitee.com/Unkaer/blog/raw/master/videos/jz.mp4' id='player' width='100%' height='600px' allowTransparency='true' allowfullscreen='true' frameborder='0' scrolling='no'> </iframe>"];
+var isiPad = navigator[__Ox20ef1[1]][__Ox20ef1[0]](/iPhone|Linux|Android|iPad|iPod|ios|iOS|Windows Phone|Phone|WebOS/i) != null;
+if (isiPad) {
+    document[__Ox20ef1[3]](__Ox20ef1[2])
+} else {
+    document[__Ox20ef1[3]](__Ox20ef1[4])
+}
